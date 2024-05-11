45ad7111321249b17f4c7105c9b8c7f407f706cd
ZhziL <161271125+ZhziL@users.noreply.github.com>
2024-05-11 21:41:49 +1000

    1
    1

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # zzho8986_9103_final
+
+## Information of Coding
+This coding using  Perlin Noise Generator from [Github Repositories](https://github.com/joeiddon/perlin), all of the point will be randomly move in a limit area and randomlly change the radius. But the colour would be using p5.js to getting each segment centre's color, to make sure using Perlin Noise would still showing the image by using random points.
+
+## Instructions on how to interact with the work
+Perlin noise randomness is used, which extracts colour from each segment, and then moves randomly within the range and randomly moves the size. You can achieve random ranges of size by changing this piece of code:
+
+```
+  draw() {
+    stroke(0);
+    
+    let dynamicRadius = radius + random(-7, 7);  
+    // dynamic radius, change -7 & 7 to control the area that point change.
+
+    fill(this.srcImgSegColour);
+    circle(this.drawXPos, this.drawYPos, dynamicRadius);
+  }
+```
+Only need to chang the 'dynamicRadius' random(-7, 7), which is the range of the radius change, then it can achieve how big/small that you want to change of the points.
+  
+This feature also supports future modifications, such as adjusting size, position, or colour based on audio, time, or other operations. To make these custom changes, simply link to additional functions or objects within this function to retrieve the necessary data.
+  
--- p5project/sketch.js
+++ p5project/sketch.js
@@ -1,83 +1,66 @@
-// We need a variable to hold our image
 let img;
-
-// We will divide the image into segments
-let numSegments = 20;
-
-// We will store the segments in an array
+let numSegments = 40;
 let segments = [];
-
-// Array to store circle objects
-let circles = [];
-
-// Let's add a variable to switch between drawing the image and the segments
 let drawSegments = true;
-
-// Let's make an object to hold the draw properties of the image
 let imgDrwPrps = {aspect: 0, width: 0, height: 0, xOffset: 0, yOffset: 0};
-
-// And a variable for the canvas aspect ratio
 let canvasAspectRatio = 0;
+let perlinNoiseOffset = 20;
 
-let radius = 15;
-
-// Global variables for noise scale and offset
-let noiseScale = 0.3;
-let noiseSpeed = 0.01;
-
-// Let's load the image from disk
+// Load the image and create segments
 function preload() {
-  img = loadImage('../p5project/asset/Mona_Lisa.jpg');
+  img = loadImage('../p5project/asset/final1.jpg');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   imgDrwPrps.aspect = img.width / img.height;
+  calculateImageDrawProps();
+
   let segmentWidth = img.width / numSegments;
   let segmentHeight = img.height / numSegments;
 
-  let positionInColumn = 0;
-  for (let segYPos = 0; segYPos < img.height; segYPos += segmentHeight) {
-    let positionInRow = 0;
-    for (let segXPos = 0; segXPos < img.width; segXPos += segmentWidth) {
+  // Create segments
+  for (let y = 0; y < numSegments; y++) {
+    for (let x = 0; x < numSegments; x++) {
+      let segXPos = x * segmentWidth;
+      let segYPos = y * segmentHeight;
       let segmentColour = img.get(segXPos + segmentWidth / 2, segYPos + segmentHeight / 2);
-      let segment = new ImageSegment(positionInColumn, positionInRow, segmentColour);
+      let segment = new ImageSegment(x, y, segmentColour);
       segments.push(segment);
-      positionInRow++;
     }
-    positionInColumn++;
   }
-  for (const segment of segments) {
-    segment.calculateSegDrawProps();
-  }
+  segments.forEach(segment => segment.calculateSegDrawProps());
 }
 
+// Draw the segments or the image
 function draw() {
-  background(10);
+  background(50);
   if (drawSegments) {
-    for (const segment of segments) {
-      segment.calculateSegDrawProps(); // Recalculate properties with noise
+    segments.forEach(segment => {
+      segment.update();
       segment.draw();
-    }
+    });
+    drawConnections();
   } else {
     image(img, imgDrwPrps.xOffset, imgDrwPrps.yOffset, imgDrwPrps.width, imgDrwPrps.height);
   }
 }
 
+// Toggle the drawing of the segments
 function keyPressed() {
-  if (key == " ") {
+  if (key === " ") {
     drawSegments = !drawSegments;
   }
 }
 
+// Resize the canvas and recalculate the drawing properties of the image
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   calculateImageDrawProps();
-  for (const segment of segments) {
-    segment.calculateSegDrawProps();
-  }
+  segments.forEach(segment => segment.calculateSegDrawProps());
 }
 
+// Calculate the drawing properties of the image
 function calculateImageDrawProps() {
   canvasAspectRatio = width / height;
   if (imgDrwPrps.aspect > canvasAspectRatio) {
@@ -98,35 +81,60 @@ function calculateImageDrawProps() {
   }
 }
 
+// Draw connections between segments
+function drawConnections() {
+  segments.forEach(segment => {
+    // Connect to right and bottom neighbors only to avoid duplicates
+    let rightNeighbor = segments.find(s => s.rowPosition === segment.rowPosition && s.columnPosition === segment.columnPosition + 1);
+    let bottomNeighbor = segments.find(s => s.rowPosition === segment.rowPosition + 1 && s.columnPosition === segment.columnPosition);
+    [rightNeighbor, bottomNeighbor].forEach(neighbor => {
+      if (neighbor) {
+        stroke(segment.srcImgSegColour);
+        strokeWeight(2);
+        line(segment.drawXPos + segment.drawWidth / 2, segment.drawYPos + segment.drawHeight / 2,
+             neighbor.drawXPos + neighbor.drawWidth / 2, neighbor.drawYPos + neighbor.drawHeight / 2);
+      }
+    });
+  });
+}
+
 class ImageSegment {
-  constructor(columnPositionInPrm, rowPositionInPrm, srcImgSegColourInPrm) {
-    this.columnPosition = columnPositionInPrm;
-    this.rowPosition = rowPositionInPrm;
-    this.srcImgSegColour = srcImgSegColourInPrm;
+
+  // Initialize the segment with its position and average colour
+  constructor(columnPosition, rowPosition, srcImgSegColour) {
+    this.columnPosition = columnPosition;
+    this.rowPosition = rowPosition;
+    this.srcImgSegColour = srcImgSegColour;
     this.drawXPos = 0;
-    this.drawYPos = 0;
+    this.drawYos = 0;
     this.drawWidth = 0;
     this.drawHeight = 0;
-    // Noise offset for organic movement
-    this.noiseOffsetX = random(1000);
-    this.noiseOffsetY = random(1000);
+    this.timeOffset = random(100);
   }
 
+  // Calculate the drawing properties of the segment
   calculateSegDrawProps() {
     this.drawWidth = imgDrwPrps.width / numSegments;
     this.drawHeight = imgDrwPrps.height / numSegments;
-    // Incorporate noise in position calculations for dynamic movement
-    this.drawXPos = (this.rowPosition + 0.5 + noise(this.noiseOffsetX) * 2 - 1) * this.drawWidth + imgDrwPrps.xOffset;
-    this.drawYPos = (this.columnPosition + 0.5 + noise(this.noiseOffsetY) * 2 - 1) * this.drawHeight + imgDrwPrps.yOffset;
-    // Update noise offsets for smooth transitions
-    this.noiseOffsetX += noiseSpeed;
-    this.noiseOffsetY += noiseSpeed;
+    this.drawXPos = this.columnPosition * this.drawWidth + imgDrwPrps.xOffset;
+    this.drawYos = this.rowPosition * this.drawHeight + imgDrwPrps.yOffset;
   }
 
+  // Update the position of the segment based on Perlin noise
+  update() {
+    const time = millis() / 1000;
+    const noiseScale = 0.1;
+    const xOffset = noise(this.columnPosition * noiseScale + time + this.timeOffset) * perlinNoiseOffset;
+    const yOffset = noise(this.rowPosition * noiseScale + time + this.timeOffset) * perlinNoiseOffset;
+    this.drawXPos = this.columnPosition * this.drawWidth + imgDrwPrps.xOffset + xOffset;
+    this.drawYos = this.rowPosition * this.drawHeight + imgDrwPrps.yOffset + yOffset;
+  }
+
+  // Draw the segment as a circle with the average colour of the segment
   draw() {
-    stroke(0);
-    let dynamicRadius = radius + random(-5, 5); // Randomness in circle size
+    noStroke();
     fill(this.srcImgSegColour);
-    circle(this.drawXPos, this.drawYPos, dynamicRadius);
+    // circle(this.drawXPos + this.drawWidth / 2, this.drawYos + this.drawHeight / 2, this.drawWidth);
+    rect(this.drawXPos, this.drawYos, this.drawWidth - 5, this.drawHeight - 5);
   }
 }
